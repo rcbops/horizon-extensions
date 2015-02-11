1c4a257eaedf34a788b79250b443e28e0aca97cc
Neill Cox <neill@ingenious.com.au>
2015-02-11 18:52:58 +1100

    Use AJAX to load Solutions

--- rackspace/heat_store/urls.py
+++ rackspace/heat_store/urls.py
@@ -3,11 +3,17 @@ try:
 except ImportError:  # Django 1.6
     from django.conf.urls import patterns, url
 
-from rackspace.heat_store.views import (IndexView, LaunchView)
+from rackspace.heat_store.views import (IndexView, LaunchView, SolutionsView)
 
 urlpatterns = patterns('',
                        url(r'^$', IndexView.as_view(), name='index'),
                        url(r'^launch/(?P<template_id>.+)$',
                            LaunchView.as_view(),
                            name='launch'),
+                       url(r'^solutions$', 
+                           SolutionsView.as_view(), 
+                           name='solutions'),
+                       url(r'^solutions/(?P<template_id>.+)$', 
+                           SolutionsView.as_view(), 
+                           name='solutions'),                       
                        )
